e3e20e0594f10dd6efd5274cfc825e932cef0240
Abhay Upadhyay <abhay281103@gmail.com>
2024-08-09 18:03:53 +0530

    UI:favicon update, button border

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head><link rel="icon" href="/favicon.png" /></head>
       <body className={inter.className}>
         <NextUIProvider>{children}</NextUIProvider>
       </body>
