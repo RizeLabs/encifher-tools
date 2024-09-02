df79c6d659ef5cad8de92290175731aae30772dd
Abhay Upadhyay <abhay281103@gmail.com>
2024-09-02 13:48:56 +0000

    dropdown for selecting which token to mint

--- app/components/Faucet.tsx
+++ app/components/Faucet.tsx
@@ -2,7 +2,7 @@
 import { Spinner } from "@nextui-org/react";
 import React, { useState } from "react";
 import Popup from "./Popup";
-
+import Image from "next/image";
 type faucetSuccessType = {
   isSuccessful: boolean;
   encifher_txid: string | undefined;
@@ -13,6 +13,13 @@ const Faucet = () => {
   const [address, setAddress] = useState<string>("");
   const [loading, setLoading] = useState(false);
   const [showModal, setShowModal] = useState(true);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [selectedToken, setSelectedToken] = useState("eBTC");
+
+  const tokens = [
+    { symbol: "eBTC", icon: "/btc.svg" },
+    { symbol: "eUSDT", icon: "/usdt.svg" },
+  ];
 
   const [success, setSuccess] = useState<faucetSuccessType>({
     isSuccessful: false,
@@ -47,8 +54,55 @@ const Faucet = () => {
 
   return (
     <div className="rounded-[32px] p-[20px] sm:p-[30px] md:p-[40px] bg-gradient-to-b from-[#00000080] to-[#212121] bg-opacity-[50%] flex flex-col space-y-[20px] sm:space-y-[25px] md:space-y-[30px] w-[90%] sm:w-[75%] md:w-[60%] lg:w-[55%] mx-auto">
-      <h1 className="text-[24px] sm:text-[28px] md:text-[32px] lg:text-[35px] font-[400]">Faucet</h1>
-
+      <h1 className="text-[24px] sm:text-[28px] md:text-[32px] lg:text-[35px] font-[400]">
+        Faucet
+      </h1>
+      <div className="flex flex-col space-y-1 items-center w-auto mb-0 relative">
+        <button
+          onClick={() => setDropdownOpen(!dropdownOpen)}
+          className="bg-gradient-to-b from-[#7754FF] to-[#643CFF] py-2 sm:py-3 px-4  text-white p-2 rounded-full flex items-center"
+        >
+          <Image
+            src={
+              tokens.find((token) => token.symbol === selectedToken)
+                ?.icon || "/eth.svg"
+            }
+            width={20}
+            height={20}
+            alt={selectedToken}
+          />
+          <span className="ml-2">{selectedToken}</span>
+          <Image
+            src={"/down.svg"}
+            width={16}
+            height={16}
+            alt="down-arrow"
+            className="ml-2"
+          />
+        </button>
+        {dropdownOpen && (
+          <ul className="absolute top-full mt-2 w-[50%]  bg-[#2D146C] text-white py-2 px-4 sm:py-3 sm:px-6 border border-gray-400 rounded-lg z-10">
+            {tokens.map((token) => (
+              <li
+                key={token.symbol}
+                className="flex items-center p-2 hover:bg-[#444444] cursor-pointer"
+                onClick={() => {
+                  setSelectedToken(token.symbol);
+                  setDropdownOpen(false);
+                }}
+              >
+                <Image
+                  src={token.icon}
+                  width={20}
+                  height={20}
+                  alt={token.symbol}
+                />
+                <span className="ml-2 text-white">{token.symbol}</span>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
       <input
         type="text"
         required
@@ -80,7 +134,6 @@ const Faucet = () => {
         />
       )}
     </div>
-
   );
 };
 
